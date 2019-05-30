0e1b432b078e3f5eab662334a43fa8d0938bae28
артем сорока <artem-soroka-2013@mail.ru>
2019-05-30 18:36:20 +0300

    Добавьте файлы проекта.

---
+++ Телефонная книга/Телефонная книга.cpp
@@ -0,0 +1,20 @@
+﻿// Телефонная книга.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct Kontact
+{
+
+};
+
+int main()
+{
+    std::cout << "Hello World!\n"; 
+}
+
+
